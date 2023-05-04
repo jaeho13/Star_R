f7e39b687241dcf0a7faf5b47659551e8f142042
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-05-04 23:32:41 +0900

    Add files via upload

---
+++ star/src/App.js
@@ -0,0 +1,12 @@
+import Clock from "./components/Clock";
+import Print from "./components/Print";
+
+function App() {
+  return (
+    <>
+    {/* <Clock /> */}
+    <Print />
+    </>
+  );
+}
+export default App;
