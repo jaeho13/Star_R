017a3fe1b8e21a0b586c11bfa1d2f5dff5834f42
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-05-08 23:18:10 +0900

    Add files via upload

--- star/src/App.js
+++ star/src/App.js
@@ -1,17 +1,20 @@
-import CleanUp from "./components/useEffect/CleanUp";
-import Update from "./components/useEffect/Update";
+// import CleanUp from "./components/useEffect/CleanUp";
+// import Update from "./components/useEffect/Update";
 // import Clock from "./components/useState/Clock";
 // import Print from "./components/useState/Print"
+// import Bank from "./components/useReducer/Bank";
+
+import Attendance from "./components/useReducer/Attendance";
 
 function App() {
   return (
     <>
     {/* <Clock />
-    <Print /> */}
-    {/* <Update /> */}
+    <Print />
+    <Update />
     <CleanUp />
-
-
+    <Bank /> */}
+    <Attendance />
 
 
     </>
